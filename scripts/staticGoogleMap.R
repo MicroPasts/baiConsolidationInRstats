5f129802065600cefa8a9fc92c66ba593a51e265
Daniel Pett <dpett@britishmuseum.org>
2016-11-29 13:32:08 +0000

    Amend script, move maps
    1. Add title for single maps
    2. Add folder for single maps
    3. Move maps
    4. Amend config files

--- scripts/staticGoogleMap.R
+++ scripts/staticGoogleMap.R
@@ -17,7 +17,8 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 # Set working directory - OSX 
 setwd("/Users/danielpett/Documents/research/micropasts/analysis/consBAI/") 
-
+# Create folder for merged files
+if (!file.exists('plots/jpegs/projects/')){dir.create('plots/jpegs/projects/')}
 # Read config file for project list
 projects <- read.csv('admin/config.csv', sep=',')
 projects <- projects$Projects
@@ -26,8 +27,9 @@ library(maps)
 library(ggmap)
 library(mapproj)
 for(project in projects){
+  print(project)
   csvname <- paste0("csv/consolidated/", project, "_cons.csv")
-  fileName <- paste0("plots/jpegs/", project, ".jpeg")
+  fileName <- paste0("plots/jpegs/projects/", project, ".jpeg")
   findspots <- read.csv(csvname, header=TRUE)
   findspots <- findspots[,c(31,32)]
   findspots <- na.omit(findspots)
@@ -36,6 +38,7 @@ for(project in projects){
     map <- get_map(center,zoom=6,source = "google")
     print(ggmap(map,extent = "device", size = c(2560,2560), darken = 0.4, scale = 2) 
           + geom_point(data=findspots, size=0.9, color="red")
+          + ggtitle(paste(paste0("Map for project: ", project)))
     )
     ggsave (fileName, dpi = 300)
   } else {
